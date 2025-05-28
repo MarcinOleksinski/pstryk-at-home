462100d51e6e83cdebc7400838ff2e397c7497e6
Marcin Oleksiński <marcin@unich.pl>
2025-05-28 13:41:04 +0200

    Poprawa rejestracji encji i obsługi rekonfiguracji (OptionsFlow) dla Home Assistant

--- custom_components/pstryk_integration/__init__.py
+++ custom_components/pstryk_integration/__init__.py
@@ -8,8 +8,12 @@ async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Pstryk integration."""
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, entry):
     """Set up Pstryk integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry):
--- custom_components/pstryk_integration/config_flow.py
+++ custom_components/pstryk_integration/config_flow.py
@@ -6,6 +6,7 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
+
 class PstrykConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Pstryk Integration."""
 
@@ -16,18 +17,33 @@ class PstrykConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(step_id="user", data_schema=self._get_schema())
 
-    async def async_step_reconfigure(self, user_input=None):
-        """Handle reconfiguration step."""
-        if user_input is not None:
-            return self.async_create_entry(title="Pstryk Integration - Reconfigured", data=user_input)
-
-        return self.async_show_form(step_id="reconfigure", data_schema=self._get_schema())
-
-    def _get_schema(self):
+    @staticmethod
+    def _get_schema():
         from homeassistant.helpers import config_validation as cv
         import voluptuous as vol
-
         return vol.Schema({
             vol.Required("api_key"): cv.string,
             vol.Optional("debug", default=False): cv.boolean,
         })
+
+    # Opcje (rekonfiguracja)
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return PstrykOptionsFlowHandler(config_entry)
+
+
+class PstrykOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        from homeassistant.helpers import config_validation as cv
+        import voluptuous as vol
+        if user_input is not None:
+            return self.async_create_entry(title="Opcje Pstryk Integration", data=user_input)
+
+        schema = vol.Schema({
+            vol.Required("api_key", default=self.config_entry.data.get("api_key", "")): cv.string,
+            vol.Optional("debug", default=self.config_entry.data.get("debug", False)): cv.boolean,
+        })
+        return self.async_show_form(step_id="init", data_schema=schema)
