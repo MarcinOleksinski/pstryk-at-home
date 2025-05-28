9a7ea65d903df79fa4124ec0ea48fe0c2bee85c7
Marcin Oleksiński <marcin@unich.pl>
2025-05-28 13:26:00 +0200

    Update sensor.py and requirements.txt for cachetools integration

--- custom_components/pstryk_integration/config_flow.py
+++ custom_components/pstryk_integration/config_flow.py
@@ -16,6 +16,13 @@ class PstrykConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(step_id="user", data_schema=self._get_schema())
 
+    async def async_step_reconfigure(self, user_input=None):
+        """Handle reconfiguration step."""
+        if user_input is not None:
+            return self.async_create_entry(title="Pstryk Integration - Reconfigured", data=user_input)
+
+        return self.async_show_form(step_id="reconfigure", data_schema=self._get_schema())
+
     def _get_schema(self):
         from homeassistant.helpers import config_validation as cv
         import voluptuous as vol
--- custom_components/pstryk_integration/sensor.py
+++ custom_components/pstryk_integration/sensor.py
@@ -3,6 +3,7 @@ Sensor platform for Pstryk Integration
 """
 
 from homeassistant.helpers.entity import Entity
+from cachetools import LRUCache
 from .api import PstrykAPIClient
 from .const import DOMAIN
 
@@ -10,13 +11,15 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Pstryk sensor platform."""
     api_key = config_entry.data["api_key"]
     client = PstrykAPIClient(api_key)
-    async_add_entities([PstrykSensor(client)])
+    cache = LRUCache(maxsize=100)
+    async_add_entities([PstrykSensor(client, cache)])
 
 class PstrykSensor(Entity):
     """Representation of a Pstryk sensor."""
 
-    def __init__(self, client: PstrykAPIClient):
+    def __init__(self, client: PstrykAPIClient, cache: LRUCache):
         self.client = client
+        self.cache = cache
         self._state = None
 
     @property
@@ -30,4 +33,5 @@ class PstrykSensor(Entity):
     async def async_update(self):
         """Fetch new state data for the sensor."""
         data = self.client.get_pricing("hour", "2025-05-27T22:00:00Z", "2025-05-28T22:00:00Z")
+        self.cache[data.get("price_net_avg")] = data
         self._state = data.get("price_net_avg")
